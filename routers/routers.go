f75d9fff089976fb426a68663ca29bc00d7594d3
G10N(nick) <87523370+nick138104@users.noreply.github.com>
2021-08-08 01:10:01 +0700

    Update routers.go

--- routers/routers.go
+++ routers/routers.go
@@ -4,21 +4,22 @@ import (
 	"gilab.com/pragmaticreviews/golang-gin-poc/users"
 	"github.com/gin-gonic/gin"
 )
+var {
+	userService service.UserService = service.New()
+	userController controller.UserController = conreoller.New(userService)
 
+}
 func GenerateRouter() *gin.Engine {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "test",
-		})
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, userController.FindAll())
 	})
 
-	router.POST("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "test",
-		})
+	router.POST("/", func(ctx *gin.Context) {
+		ctx.JSON(200, userController.Save(ctx))
 	})
+
 	return router
 }
 
@@ -35,9 +36,33 @@ func New() UserService {
 	return &userService{}
 }
 
-func (s *userService) Save(user users.User) users.User {
+func (service *userService) Save(user users.User) users.User {
 	s.users = append(s.users, user)
 }
-func (s *userService) FindAll() []users.User {
+func (service *userService) FindAll() []users.User {
 	return s.users
+}
+
+type UserController interface {
+	Save(users.User) users.User
+	FindAll() []users.User
+}
+
+type controller struct {
+	service service.UserService
+}
+
+func New(service service.UserService) UserController {
+	return &controller{
+		service: service,
+	}
+}
+func (c *controller) FindAll() []users.User{
+	return c.service.FindAll()
+}
+func (c *controller) Save(ctx *gin.Context) users.User{
+	var user users.User
+	ctx.BindJSON(&user)
+	c.service.Save()
+	return user
 }
